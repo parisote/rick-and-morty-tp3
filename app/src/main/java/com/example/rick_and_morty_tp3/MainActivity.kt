94628eb1be2e067ce81a6e8e0f38d9e90fc2807d
parisote <detomas@gmail.com>
2022-11-01 20:18:04 -0300

    first

---
+++ app/src/main/java/com/example/rick_and_morty_tp3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rick_and_morty_tp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
