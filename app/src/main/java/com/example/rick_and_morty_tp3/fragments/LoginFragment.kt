93a80dd232e6d6c1688cc7869f7345e70a10b355
Emiliano Goyeneche <emilianogoyeneche@gmail.com>
2022-11-02 21:48:21 -0300

    login action to home
    login action to home
    
    Co-Authored-By: Tomas <tomas.climente@mercadolibre.com>
    Co-Authored-By: Ariel Butrón <88804984+aributron@users.noreply.github.com>
    Co-Authored-By: tomasperez99 <69942676+tomasperez99@users.noreply.github.com>
    Co-Authored-By: Juan Martin <18727639+harushishi@users.noreply.github.com>
    Co-Authored-By: Nicolás <59515493+nicocalezich@users.noreply.github.com>

--- app/src/main/java/com/example/rick_and_morty_tp3/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/rick_and_morty_tp3/fragments/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import com.example.rick_and_morty_tp3.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,6 +25,9 @@ class LoginFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var userEditText: EditText
+    private lateinit var passwordEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -36,6 +42,27 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+        val continueButton = view.findViewById<Button>(R.id.button_login)
+        userEditText = view.findViewById(R.id.username_input)
+        passwordEditText = view.findViewById(R.id.password_input)
+
+        // Establezco un listener para escuchar cualquier click en el boton
+        /*continueButton.setOnClickListener {
+
+            // Navego hacia la home
+            navController.navigate(R.id.action_loginFragment_to_homeFragment)
+                /*LoginFragmentDirections.actionLoginFragmentToHomeFragment(
+                    userEditText.text.toString(),
+                    passwordEditText.text.toString()
+                )
+            )*/
+        }*/
     }
 
     companion object {
