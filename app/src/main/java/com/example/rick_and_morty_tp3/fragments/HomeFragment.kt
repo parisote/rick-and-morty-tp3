493e5dbd13205022eebbff602b254b798dbada60
aributron <ari.butron95@gmail.com>
2022-11-04 00:18:58 -0300

    agregando sharedPreferences sin terminar

--- app/src/main/java/com/example/rick_and_morty_tp3/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/rick_and_morty_tp3/fragments/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.rick_and_morty_tp3.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +10,15 @@ import android.view.ViewGroup
 import com.example.rick_and_morty_tp3.R
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rick_and_morty_tp3.adapter.CharacterAdapter
 import com.example.rick_and_morty_tp3.model.Character
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,9 +38,10 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        loadData()
+
+        button_login.setOnClickListener {
+            saveData()
         }
     }
 
@@ -48,11 +55,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // title = view.findViewById(R.id.txtHello)
-        // Pongo el nombre del usuario en el titulo.
-        // Advertencia: Al momento de mostrar un texto al usuario siempre usar un String resource. Nunca hardcodear de
-        // esta manera.
-        // title.text = "Hola, ${param1}"
+        title = view.findViewById(R.id.txtHello)
 
         // lista de personajes hardcodeada
         val person = Character("Jorge", "Alive", "https://rickandmortyapi.com/api/character/avatar/21.jpeg")
@@ -68,23 +71,25 @@ class HomeFragment : Fragment() {
         recyclerView.adapter = adapter
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun saveData() {
+        val insertedText = username_input.text.toString()
+        title.text = "Hola, ${insertedText}"
+        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.apply() {
+            putString("STRING_KEY", insertedText)
+
+        }.apply()
+
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
     }
+
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val savedString = sharedPreferences.getString("STRING_KEY", null)
+
+        title.text = savedString
+
+    }
+
 }
